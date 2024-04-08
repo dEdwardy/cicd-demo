95e6ca639fd3ce302dc1756084702ece6bf38ae9
edw4rd <957196958@qq.com>
2024-04-08 21:30:44 +0800

    feat: page

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: 'Index',
+        meta: {
+            title: '首页',
+            keepAlive: true,
+            requireAuth: true,
+        },
+        component: () => import('../views/HomePage.vue'),
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+export default router
